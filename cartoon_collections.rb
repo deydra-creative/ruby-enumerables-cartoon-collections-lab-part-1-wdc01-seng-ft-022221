7d853eca6d2c1b3abc8ee613a8b9064fb7486364
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-08 18:10:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,13 +7,7 @@ end
 
 def list_dwarves(array)
 array.each_with_index do |name,index|
-    print index+1 
+    p index+1 
     puts ". " + name
 end 
-
-array.each_with_index do |name,index|
-    puts (index+1).to_s + ". " + name
-    print index+1 
-    puts ". " + name
-    #puts (index+1).to_s + ". " + name
-  end
+end 
